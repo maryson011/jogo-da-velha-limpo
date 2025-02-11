26e0959e9e10e7d95a51ef3061d20314d1a5f8ca
maryson011 <maryson011@gmail.com>
2025-02-11 09:29:46 -0300

    feat: add border area

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,18 +1,13 @@
 'use client'
 
-import { PlayerType } from "@/core/src";
-import CellArea from "./components/game/CellArea";
+import BoardArea from "./components/game/BorderArea";
 
 //import { Board } from '../core/src/index'
 
 export default function Home() {
   return (
     <div>
-      <CellArea type={PlayerType.O} selected />
-      <CellArea type={PlayerType.X} selected />
-      <CellArea type={PlayerType.O} />
-      <CellArea type={PlayerType.X} />
-      <CellArea />
+      <BoardArea />
     </div>
   );
 }
