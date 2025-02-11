e45eb04f2bbf47adb4d0e2a97c0aaaff1c78353f
maryson011 <maryson011@gmail.com>
2025-02-11 08:56:51 -0300

    feat: add game context

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,16 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Outfit } from "next/font/google";
 import "./globals.css";
+import { GameProvider } from "@/contexts/GameContext";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const outfit = Outfit({
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,9 +21,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${outfit.className}`}
       >
-        {children}
+        <div className="flex justify-center items-center h-screen">
+          <GameProvider>
+            {children}
+          </GameProvider>
+        </div>
       </body>
     </html>
   );
---
+++ frontend/src/contexts/GameContext.tsx
@@ -0,0 +1,57 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+'use client'
+import { Board, Game, GameResult, Player, PlayerType } from "../core/src/index"
+import { createContext, useState } from "react"
+
+interface GameContextProps {
+    ties: number
+    player1: Player
+    player2: Player
+    currentPlayer: Player
+    board: Board
+    result: GameResult
+    addMove: (row: number, col: number) => void
+    restart: () => void
+    clear: () => void
+}
+
+const GameContext = createContext<GameContextProps>({} as any)
+
+export function GameProvider(props: any) {
+    const [game, setGame] = useState<Game>(
+        Game.create(
+            new Player('P1', PlayerType.X),
+            new Player('P2', PlayerType.O),
+        )
+    )
+
+    function addMove(row: number, col: number) {
+        setGame(game.addMove(row, col))
+    }
+
+    function restart() {
+        setGame(game.nextRound())
+    }
+
+    function clear() {
+        setGame(game.clear())
+    }
+
+    return (
+        <GameContext.Provider value={{
+            ties: game.ties,
+            player1: game.player1,
+            player2: game.player2,
+            currentPlayer: game.currentPlayer,
+            board: game.board,
+            result: game.result,
+            addMove,
+            restart,
+            clear,
+        }}>
+            {props.children}
+        </GameContext.Provider>
+    )
+}
+
+export default GameContext
